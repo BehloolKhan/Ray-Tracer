8452d0893406e1d9c14f2a4af72d003167e85ee6
Behlool Khan <behloolk2020@gmail.com>
2026-06-10 18:27:09 +0100

    these are the constants used in this book, such as viewp port size and distance d

---
+++ Constants.h
@@ -0,0 +1,13 @@
+//The file will include the constants used in this ray tracer
+//
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+int CANVAS_WIDTH = 500;
+int CANVAS_HEIGHT = 500;
+
+int VIEWPORT_HEIGHT = 1;
+int VIEWPORT_WIDTH = 1;
+
+int PROJECTTION_PLANE_D = 1;
+#endif
