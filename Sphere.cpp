1f6e310b63deb0179cc0ee957bdec8577daa55cd
Behlool Khan <behloolk2020@gmail.com>
2026-06-12 00:00:45 +0100

    sperating the constructor of sphere to cpp file

---
+++ Sphere.cpp
@@ -0,0 +1,8 @@
+#include "Sphere.h"
+
+
+Sphere::Sphere(Vec3 center_, float radius_, std::tuple<int, int, int> color_) {
+	this->center = center_;
+	this->radius = radius_;
+	this->color = color_;
+}
