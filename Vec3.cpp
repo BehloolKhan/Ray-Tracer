8efa7527cff9ed9ee62a693ee05006b277b68118
Behlool Khan <behloolk2020@gmail.com>
2026-06-11 14:11:08 +0100

    this file will contain the method defintions inside the Vec3 class

---
+++ Vec3.cpp
@@ -0,0 +1,23 @@
+//This file will store the method defintions of the class Vec3
+//
+#include "Vec3.h"
+
+Vec3::Vec3(float x_, float y_, float z_) {
+	this->x = x_;
+	this->y = y_;
+	this->z = z_;
+}
+
+Vec3::Vec3() {
+	x = 0;
+	y = 0;
+	z = 0;
+}
+
+Vec3 Vec3::operator+(const Vec3& otherVec3) const {
+	return Vec3(this->x + otherVec3.x, this->y + otherVec3.y, this->z + otherVec3.z);
+}
+
+float Vec3::dot(const Vec3& vec1, const Vec3& vec2) {
+	return ((vec1.x*vec2.x) + (vec1.y + vec2.y) + (vec1.z + vec1.z));
+}
