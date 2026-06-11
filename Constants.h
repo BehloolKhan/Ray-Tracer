b897069efcad9d0c39312aa038bf7ebd89551e98
Behlool Khan <behloolk2020@gmail.com>
2026-06-11 12:52:01 +0100

    changing view port and canvas dimensions to prevent integer devision

--- Constants.h
+++ Constants.h
@@ -5,11 +5,11 @@
 
 #include "Vec3.h"
 
-float CANVAS_WIDTH = 500;
-float CANVAS_HEIGHT = 500;
+int CANVAS_WIDTH = 500;
+int CANVAS_HEIGHT = 500;
 
-float VIEWPORT_HEIGHT = 1;
-float VIEWPORT_WIDTH = 1;
+int VIEWPORT_HEIGHT = 1;
+int VIEWPORT_WIDTH = 1;
 
 int PROJECTTION_PLANE_D = 1;
 Vec3 origin = Vec3(0, 0, 0);
