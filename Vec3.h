6633f4a0f91d7fc2e28e18091ea04e7a0877b1f9
Behlool Khan <behloolk2020@gmail.com>
2026-06-11 14:20:23 +0100

    adding the - operator method so we can subtract two methods

--- Vec3.h
+++ Vec3.h
@@ -7,6 +7,19 @@ struct Vec3 {
 	float x;
 	float y;
 	float z;
+	//constructor methods
+	Vec3(float x_, float y_, float z_);
+	Vec3();
+
+	//addition one two vectors
+	Vec3 operator+(const Vec3& otherVec3) const;
+
+
+	//addition one two vectors
+	Vec3 operator-(const Vec3& otherVec3) const;
+
+	//returns the dot product of two vectors
+	static float dot(const Vec3& vec1, const Vec3& vec2);
 
 	Vec3(float x_, float y_, float z_) {
 		this->x = x_;
