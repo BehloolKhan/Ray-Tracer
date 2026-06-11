e3cd014d519978f82518426a5337e3126d6b5b90
Behlool Khan <behloolk2020@gmail.com>
2026-06-12 00:30:54 +0100

    making sure header file only contains method signature

--- Sphere.h
+++ Sphere.h
@@ -8,11 +8,6 @@ struct Sphere {
 	Vec3 center;
 	float radius;
 	std::tuple<int, int, int> color; 
-
-	Sphere(Vec3 center_, float radius_, std::tuple<int, int, int> color_) {
-		this->center = center_;
-		this->radius = radius_;
-		this->color = color_;
-	}
+	Sphere(Vec3 center_, float radius_, std::tuple<int, int, int> color_);
 };
-#endif // !SPHERE_H
+#endif
