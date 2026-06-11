b38197446e3931d63485c6588b51802fb13786b9
Behlool Khan <behloolk2020@gmail.com>
2026-06-11 12:58:29 +0100

    this file will store the method defintions

---
+++ methods.cpp
@@ -0,0 +1,8 @@
+//This will include the function/procedure definitons for all the functions/procedures used in the book
+//
+#ifndef METHODS_CPP
+#define METHODS_CPP
+
+#include "Constants.h"
+
+#endif
