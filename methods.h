69ad24dfcded08db654541a59f60cc1e6f4c3bee
Behlool Khan <behloolk2020@gmail.com>
2026-06-11 22:58:59 +0100

    this header file contains the major methods used in first part of the book - Basic RayTracing

--- methods.h
+++ methods.h
@@ -1,7 +1,24 @@
-//header file containg method defintions and declerations
+//header file containg method declerations
 //
 #ifndef METHODS_H
 #define METHODS_H
 
+#include <tuple>
+#include "Vec3.h"
+#include "Sphere.h"
+#include <vector>
 
+//method returns the two t values where a ray interserct a given sphere if it does so
+//if ray doesnt interesect sphere, infinity is returned
+//
+std::tuple<float, float> IntersectRaySphere(Vec3& O_, Vec3& D, Sphere sphere);
+
+//method computes the intersection of the ray with every sphere
+//returns the color of the sphere at the nearest interaction inside range
+//
+std::tuple<int, int, int>TraceRay(Vec3& O_, Vec3& D, float t_min, float t_max, std::vector<Sphere>& Spheres);
+
+//method sets up the scene, i.e sets up the spheres and their correponding colors and co-ordinates
+//
+std::vector<Sphere> setUpScene();
 #endif
