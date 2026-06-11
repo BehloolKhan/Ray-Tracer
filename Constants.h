2a869c38e74314ce551cd5b5724be83b7e7a16e4
Behlool Khan <behloolk2020@gmail.com>
2026-06-11 23:15:22 +0100

    adding the background colour, and the canvas/viewport dimensions

--- Constants.h
+++ Constants.h
@@ -4,14 +4,15 @@
 #define CONSTANTS_H
 
 #include "Vec3.h"
+#include <tuple>
 
-int CANVAS_WIDTH = 500;
-int CANVAS_HEIGHT = 500;
+float CANVAS_WIDTH = 500;
+float CANVAS_HEIGHT = 500;
 
-int VIEWPORT_HEIGHT = 1;
-int VIEWPORT_WIDTH = 1;
+float VIEWPORT_HEIGHT = 1;
+float VIEWPORT_WIDTH = 1;
 
 int PROJECTTION_PLANE_D = 1;
 Vec3 origin = Vec3(0, 0, 0);
-
+std::tuple<int, int, int> BACKGROUND_COLOUR(255, 255, 255);
 #endif
