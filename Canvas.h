5d458bb1ec7ee81e1c80ab9ce962db5f738cda2a
Behlool Khan <behloolk2020@gmail.com>
2026-06-12 19:29:21 +0100

    seperating the method function decleration from defitinion and

--- Canvas.h
+++ Canvas.h
@@ -1,9 +1,19 @@
 #ifndef CANVAS_H
 #define CANVAS_H
+#include <fstream>
+#include <tuple>
+#include "Constants.h"
 
 class Canvas {
+public:
+	//constructor
+	Canvas();
+	void putPixel(int x, int y, std::tuple<int, int, int>& color);
+	void writeToFile();
+	~Canvas();
+
 private:
-	int x;
-	int y;
+	std::ofstream imageFile;
+	std::tuple<int, int, int>* colors = new std::tuple<int, int, int>[(int)CANVAS_WIDTH * (int)CANVAS_HEIGHT];
 };
 #endif 
