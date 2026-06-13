ccaa1e3ae155f78e3a734a51a6069aea56c98998
Behlool Khan <behloolk2020@gmail.com>
2026-06-13 17:24:25 +0100

    printing final message to let user know if program has run successfully, only going to but not including maximum x and y co-ordinates because it means the loop would run 101 times for x and y, causing us to go out of bounds for the array we want to store colour in

--- Source.cpp
+++ Source.cpp
@@ -16,15 +16,17 @@ int main() {
 	std::vector<Sphere> Spheres = setUpScene();
 	Canvas canvas{}; //use curly brace to initialise Canvas object;
 
-	for (int x = -CANVAS_WIDTH / 2; x <= CANVAS_WIDTH / 2; ++x) {
-		for (int y = -CANVAS_HEIGHT / 2; y <= CANVAS_HEIGHT / 2; ++y) {
+	for (int x = -CANVAS_WIDTH / 2; x < CANVAS_WIDTH / 2; ++x) {
+		for (int y = -CANVAS_HEIGHT / 2; y < CANVAS_HEIGHT / 2; ++y) {
+			std::cout << "current x: " << x << " Current y: " << y << '\n';
 			Vec3 V = canvasToViewPort(x, y);
 			Vec3 D = V - origin;
 			float posInf = std::numeric_limits<float>::infinity();
 			std::tuple<int, int, int> color = TraceRay(origin, D, 1.0, posInf, Spheres);
+			std::cout << "color calculated\n";
 			canvas.putPixel(x, y, color);
 		}
 	}
-
+	std::cout << "rendering complete";
 	canvas.writeToFile();
 }
