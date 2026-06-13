78396703fe712146142e7844dba4645c578194d8
Behlool Khan <behloolk2020@gmail.com>
2026-06-13 17:26:04 +0100

    chaning the constants to inline because they have been included multiple times across different files - gotta tell compiler to merge the defintions

--- Constants.h
+++ Constants.h
@@ -6,13 +6,13 @@
 #include "Vec3.h"
 #include <tuple>
 
-float CANVAS_WIDTH = 500;
-float CANVAS_HEIGHT = 500;
+inline constexpr float CANVAS_WIDTH = 100;
+inline constexpr float CANVAS_HEIGHT = 100;
 
-float VIEWPORT_HEIGHT = 1;
-float VIEWPORT_WIDTH = 1;
+inline constexpr float VIEWPORT_HEIGHT = 1;
+inline constexpr float VIEWPORT_WIDTH = 1;
 
-int PROJECTTION_PLANE_D = 1;
-Vec3 origin = Vec3(0, 0, 0);
-std::tuple<int, int, int> BACKGROUND_COLOUR(255, 255, 255);
+inline constexpr int PROJECTTION_PLANE_D = 1;
+inline Vec3 origin = Vec3(0, 0, 0);
+inline std::tuple<int, int, int> BACKGROUND_COLOUR(255, 255, 255);
 #endif
