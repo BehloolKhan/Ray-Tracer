272960ba73939b475bacee844873f585af9cc2e7
Behlool Khan <behloolk2020@gmail.com>
2026-07-02 12:54:06 +0100

    editing the values of the origin and adding the rotation matrix

--- core/Constants.h
+++ core/Constants.h
@@ -13,6 +13,11 @@ inline constexpr float VIEWPORT_HEIGHT = 1;
 inline constexpr float VIEWPORT_WIDTH = 1;
 
 inline constexpr int PROJECTTION_PLANE_D = 1;
-inline Vec3 origin = Vec3(1, 1, 1);
+inline Vec3 origin = Vec3(0, 0, 0);
 inline std::tuple<int, int, int> BACKGROUND_COLOUR(0, 0, 0);
+
+inline float rotationMatrix[3][3] = {
+	{0.7071, 0.000, 0.7071},
+	{-0.4082, 0.8165, 0.4082},
+	{-0.5774, -0.5774, 0.5774} };
 #endif
