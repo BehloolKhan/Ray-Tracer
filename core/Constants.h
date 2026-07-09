97c1a116db3416d888dc1331d2f42c80784be426
Behlool Khan <behloolk2020@gmail.com>
2026-07-09 18:57:16 +0100

    chaning the matrix to identity to make camera not rotate and stay the same

--- core/Constants.h
+++ core/Constants.h
@@ -17,7 +17,7 @@ inline Vec3 origin = Vec3(0, 0, 0);
 inline std::tuple<int, int, int> BACKGROUND_COLOUR(0, 0, 0);
 
 inline float rotationMatrix[3][3] = {
-	{0.7071, 0.000, 0.7071},
-	{-0.4082, 0.8165, 0.4082},
-	{-0.5774, -0.5774, 0.5774} };
+	{1.0, 0.0, 0.0},
+	{0.0, 1.0, 0.0},
+	{0.0, 0.0, 1.0}};
 #endif
