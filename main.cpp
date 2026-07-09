75f4eb30b2cb77295776bf509d4750efc717607b
Behlool Khan <behloolk2020@gmail.com>
2026-07-09 18:58:04 +0100

    using multithreading to paralleise ray tracer

--- main.cpp
+++ main.cpp
@@ -8,6 +8,20 @@
 #include <vector>
 #include "core/Canvas.h"
 #include "scene/Scene.h"
+#include <thread>
+
+void partOfCanvas(int start_X, int end_X, Scene& scene, Canvas& canvas) {
+
+	for (int x = start_X; x < end_X; ++x) {
+		for (int y = -CANVAS_HEIGHT / 2; y < CANVAS_HEIGHT / 2; ++y) {
+			Vec3 V = canvasToViewPort(x, y);
+			Vec3 D = rotationMatrix * V;
+			float posInf = std::numeric_limits<float>::infinity();
+			std::tuple<int, int, int> color = TraceRay(origin, D, 1.0, posInf, scene, 3);
+			canvas.putPixel(x, y, color);
+		}
+	}
+}
 
 int main() {
 	std::cout << "Hello World" << '\n';
@@ -18,15 +32,30 @@ int main() {
 	Canvas canvas{}; //use curly brace to initialise Canvas object;
 
 	scene.setUpScene(); //get the information we need;
-
-	for (int x = -CANVAS_WIDTH / 2; x < CANVAS_WIDTH / 2; ++x) {
-		for (int y = -CANVAS_HEIGHT / 2; y < CANVAS_HEIGHT / 2; ++y) {
-			Vec3 V = canvasToViewPort(x, y);
-			Vec3 D = rotationMatrix*V;
-			float posInf = std::numeric_limits<float>::infinity();
-			std::tuple<int, int, int> color = TraceRay(origin, D, 1.0, posInf, scene, 3);
-			canvas.putPixel(x, y, color);
-		}
+	unsigned int num_threads = std::thread::hardware_concurrency();
+	if (num_threads == 0) {
+		num_threads = 2;
 	}
+
+	std::vector<std::thread> vectorThreads;
+
+	int chunk = (int)(CANVAS_WIDTH/ num_threads);
+	int start_X = -CANVAS_WIDTH / 2;
+
+	//creating threads
+	for (int i = 1; i <= num_threads; ++i) {
+		vectorThreads.push_back( std::thread(partOfCanvas, start_X, start_X+chunk, std::ref(scene), std::ref(canvas)) );
+		start_X += chunk;
+	}
+
+	if (start_X < ((int)CANVAS_WIDTH / 2)) {
+		vectorThreads.push_back( std::thread(partOfCanvas, start_X, (int)CANVAS_WIDTH / 2, std::ref(scene), std::ref(canvas)) );
+	}
+
+	//joining threads
+	for (std::thread& item : vectorThreads) {
+		item.join();
+	}
+
 	canvas.writeToFile();
 }
